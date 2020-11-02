c9efae94ccf66b9f206549db337a0c3661ae0a3c
unknown <tompsherman@gmail.com>
2020-11-02 18:29:30 -0500

    figured out knex stuff was stretch

--- api/server.js
+++ api/server.js
@@ -8,21 +8,10 @@ const server = express();
 
 server.use(express.json());
 
-const Accounts = {
-    getAll() {
-        return db('accounts')
-    }
-}
+server.use("/api/accounts", accountRouter)
+
 server.get("/test", (req,res)=>{ 
     res.status(200).json({message: "allo allo allo"})
- })
-
-server.get("/", (req,res)=>{ 
-    Accounts.getAll()
-    .then(accounts => {
-        res.status(200).json(accounts)
-    })
-    .catch(error => res.status(500).json({message: `${error.message}; ${error.stack}`}))
  })
 
 module.exports = server;
